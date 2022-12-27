28ea1405fb751cce42a7b8986ee82c3395bf9959
Tyler Schumacher <tschuma3@uccs.edu>
2022-12-27 15:25:50 -0700

    Added MLP Quick Sort

--- Using Matplotlib/Sorting(MPL).py
+++ Using Matplotlib/Sorting(MPL).py
@@ -7,6 +7,7 @@ import random
 import numpy as np
 from matplotlib import pyplot as plt, animation
 
+#Bubble Sort Algorithm
 def bubble_Sort(lengths):
      
     #Iterates through the lengths
@@ -21,18 +22,55 @@ def bubble_Sort(lengths):
             
             yield lengths
 
+#Quick Sort Algorithm
+def quick_Sort(lengths, low, high):
+    if low < high:
+
+        #Finds the element
+        pi = partition(lengths, low, high)
+
+        #For the left side
+        quick_Sort(lengths, low, pi - 1)
+
+        #For the right side
+        quick_Sort(lengths, pi + 1, high)
+
+def partition(lengths, low, high):
+    #Sets the pivot
+    pivot = lengths[high]
+
+    #Pointer for the greater element
+    i = low - 1
+
+    #Iterates through the elements and compares the pivot
+    for j in range(low, high):
+        if lengths[j] <= pivot:
+
+            #Swaps the elements
+            i += 1
+            lengths[i], lengths[j] = lengths[j],lengths[i]
+
+        #Swaps the pivot with high
+        lengths[i + 1], lengths[high] = lengths[high], lengths[i + 1]
+
+    #Returns the position
+    return i + 1
+
 #Visualizers
 def visualize():
     n = 30
     lengths = np.random.randint(1, 275, n)
+    low = 0
+    high = len(lengths)
     random.shuffle(lengths)
 
     #Creates a generator object that contains the lengths
-    generator = bubble_Sort(lengths)
+    generator_Bubble = bubble_Sort(lengths)
+    generator_Quick = quick_Sort(lengths, low, high - 1)
 
     #Creates the figures and subplots
     figure, axis = plt.subplots()
-    axis.set_title("Bubble Sort O(n\N{SUPERSCRIPT TWO})")
+    axis.set_title("Sort O(n\N{SUPERSCRIPT TWO})")
     bar_Sub = axis.bar(range(len(lengths)), lengths, align="edge")
 
     #Sets the max for the x-axis
@@ -52,7 +90,7 @@ def visualize():
         figure, 
         func=update,
         fargs=(bar_Sub, iteration),
-        frames=generator,
+        frames=generator_Bubble,
         repeat=True,
         blit=False,
         interval=15,
